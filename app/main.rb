bb05f70ac246793aa85811f8d407d83907416554
Gness Erquint <gness.na@gmail.com>
2021-11-02 11:36:52 +0300

    Major comments slimdown.

--- app/main.rb
+++ app/main.rb
@@ -6,107 +6,6 @@ require '/lib/xenovmath/vectormath.rb'
 require '/lib/giatros/frametimer.rb'
 require '/app/hood.rb'
 
-# https://www.nagwa.com/en/explainers/175169159270/
-# http://www.sunshine2k.de/coding/java/PointOnLine/PointOnLine.html.
-# https://falstad.com/dotproduct/
-# https://mathworld.wolfram.com/search/?query=vector+determinant&x=0&y=0
-# https://www.xarg.org/book/linear-algebra/2d-perp-product/
-# https://books.physics.oregonstate.edu/GMM/book-1.html
-# https://books.physics.oregonstate.edu/LinAlg/book-1.html
-
-# args.inputs.mouse.click && args.inputs.mouse.button_right
-# if args.inputs.mouse.click
-  # puts args.inputs.mouse.class.instance_methods
-# end
-# args.inputs.mouse&.button_right
-
-=begin
-class Array
-  def v_inv # Inversion. → Vector
-    return self.map(&:-@)
-  end
-  def v_add other # Vector addition. → Vector
-    return self.zip(other).map {|pair| pair.first + pair.last}
-    # return self.zip(other).map {|v_a, v_b| v_a + v_b}
-    # return self.zip(other).map {|pair| pair.inject(&:+)}
-  end
-  def v_sub other # Vector subtraction. → Vector
-    return self.v_add other.v_inv
-  end
-  def v_smult scalar # Scalar multiplication. Scaling. → Vector
-    return self.map {|v| v * scalar}
-  end
-  def v_sdiv scalar # Scalar division. → Vector
-    return self.v_smult (1.0 / scalar)
-  end
-  def v_cmult other # Complex multiplication. → Vector
-    return [
-      self.first * other.first - self.last * other.last,
-      self.first * other.last  + self.last * other.first
-    ]
-  end
-  def v_cdiv other # Complex division. → Vector
-    return [
-      (self.first * other.first + self.last * other.last) /
-        (other.first ** 2 + other.last ** 2),
-      (self.last * other.first - self.first * other.last) /
-        (other.first ** 2 + other.last ** 2)
-    ]
-  end
-  def v_mag # Magnitude. Norm. → Scalar
-    return Math.sqrt((self.first ** 2) + (self.last ** 2))
-    # return Math.sqrt(self.inject(0) {|sum, a| sum += a ** 2})
-  end
-  def v_unit # Normalize vector. → Vector
-    return self.v_sdiv self.v_mag
-  end
-  def v_setmag scalar # Sets arbitrary magnitude. → Vector
-    return self.v_unit.v_smult scalar
-  end
-  def v_theta other # Angle in radians. → Scalar
-    return Math.acos(self.v_costheta other)
-  end
-  def v_costheta other # Cosine of angle in radians. → Scalar
-    return (self.v_dprod other) / (self.v_mag * other.v_mag)
-    # v1.unit.dot(v2.unit) == Math.cos(v1.theta(v2))
-  end
-  def v_perp # Perpendicular. → Vector
-    return [-self.y, self.x]
-  end
-  def v_dprod other # Dot product. → Scalar
-    raise 'Size mismatch!' unless self.size == other.size
-    return self.zip(other).inject(0) {|sum, (v_a, v_b)| sum += v_a * v_b}
-  end
-  def v_pdprod other # Perp dot product. → Scalar
-    return self.x * other.y - self.y * other.x
-    # return self.v_perp.v_dprod other
-    # return Math.sqrt((self.v_mag ** 2) * (other.v_mag ** 2) - ((self.v_dprod other) ** 2))
-    # return Math.sqrt(((self.first ** 2) + (self.last ** 2)) * ((other.first ** 2) + (other.last ** 2)) - ((self.v_dprod other) ** 2))
-  end
-end
-=end
-
-    # raise 'Not vec2!' unless self.size == 2
-    # raise 'Malformed vec2!' unless
-      # self.x.is_a?(Float) || self.x.is_a?(Fixnum) &&
-      # self.y.is_a?(Float) || self.y.is_a?(Fixnum)
-
-# (a.v_pdprod b) ** 2 + (a.v_dprod b) ** 2 = (a.v_mag ** 2) * (b.v_mag ** 2)
-# a.v_pdprod b = Math.sqrt((a.v_mag ** 2) * (b.v_mag ** 2) - ((a.v_dprod b) ** 2))
-# a.v_pdprod b = Math.sqrt((Math.sqrt(a.inject(0) {|sum, a| sum += a ** 2}) ** 2) * (Math.sqrt(b.inject(0) {|sum, a| sum += a ** 2}) ** 2) - ((a.v_dprod b) ** 2))
-# sqrt((a.inject(0) {|sum, a| sum += a ^ 2}) * (b.inject(0) {|sum, a| sum += a ^ 2}) - ((a.v_dprod b) ^ 2))
-# \sqrt{\left(a_{x}^{2}+a_{y}^{2}\right)\left(b_{x}^{2}+b_{y}^{2}\right)-\left(a·b\right)^{2}}
-
-# To be benchmarked.
-# costheta = Math.cos(Math.acos( v1.dot(v2) / (v1.abs * v2.abs) ))
-# costheta = Math.cos(Math.acos( v1.unit.dot(v2.unit) ))
-# costheta = Math.cos( (Math.atan2(v1.x, v1.y) - Math.atan2(v2.x, v2.y)).abs )
-# proj_bound = (0..1).include?(proj_magnitude / v1.abs)
-# v3 = v1.unit.scale proj_magnitude # "Yellow" point.
-# v3 = v1.v_setmag(v2.abs * v1.v_costheta(v2)) # "Yellow" point.
-# proj_bound = (0..(v1.abs ** 2)).include?(v1.dot(v3))
-# proj_bound = (0..(v1.dot(v1))).include?(v1.dot(v3))
-
 def tick args
   if args.state.tick_count == 0
     $points = {
@@ -134,10 +33,7 @@ def tick args
       $c_radius = 0 if $c_radius.negative?
     end
     
-    # op = nil
-    # 1e4.truncate.times do
     op = ortho_proj $points[:lseg], $points[:rseg], $points[:circle]
-    # end
     
     $points[:proj] = op[:global][:projected]
     
@@ -233,7 +129,6 @@ def tick args
       }, { # Angle degrees readout.
         primitive_marker: :label,
         x: $points[:lseg].x + $p_size / 2, y: $points[:lseg].y + $p_size / 2,
-        # text: "#{(op[:theta] / DEG2RAD).round(2)}°",
         text: "%.2f°" % (op[:theta] / DEG2RAD),
         r: 213, g: 213, b: 213
       }, { # Circle sprite.
@@ -248,17 +143,3 @@ def tick args
     ]
   end
 end
-
-=begin
-  $a, $b, $c = [0.8, 1.1], [9, 0.6], 100000
-  
-  if args.state.tick_count == 500
-    time = Time.now; $c.times{$a.v_pdprod1 $b}; puts Time.now - time
-  end
-  if args.state.tick_count == 501
-    time = Time.now; $c.times{$a.v_pdprod2 $b}; puts Time.now - time
-  end
-  if args.state.tick_count == 502
-    time = Time.now; $c.times{$a.v_pdprod3 $b}; puts Time.now - time
-  end
-=end
